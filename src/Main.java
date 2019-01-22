f79d72d269b367ecd49099adea923adb3c0df4c8
justii1306 <35031460+justii1306@users.noreply.github.com>
2019-01-22 14:40:05 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args){
-        //10000, 0, 1
+        //10000 0 1 - przykładowe parametry programu
         try {
-            RandomNumbers test = new RandomNumbers(
+            RandomNumbers test = new RandomNumbers( //Tworzymy obiekt i przekazujemy mu parametry programu
                     Integer.parseInt(args[0]),
                     Integer.parseInt(args[1]),
-                    Integer.parseInt(args[2]));
-            test.closeDataStreams();
+                    Integer.parseInt(args[2]));     //Większość metod wywołuje się sama przez konstruktor
+            test.closeDataStreams(); //Zamykamy strumienie
         } catch (FileNotFoundException e){
             System.out.println(e.getMessage());
         } catch (IOException e){
